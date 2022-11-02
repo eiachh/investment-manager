bbdabbde8a9b27280f93f20d852b1cb5cd5fdb97
Costinha96 <diogodarc1996@gmail.com>
2022-11-02 21:56:38 +0000

    initial commit - added some kind of answer

--- main.py
+++ main.py
@@ -8,9 +8,50 @@ class investmentManager():
     def __init__(self):
         self.request_data = ''
 
-    def getPreferredInvestmentJson(self):
-        return {'Result': 'None'}
 
+    def getPreferredInvestmentJson(self):
+        nextInvestment = self.getManagerOptions()
+        return nextInvestment
+
+
+    def getManagerOptions(self):
+        researchManagerOption = self.request_data['researchManager']    
+        buildingManagerOption = self.request_data['buildingManager']
+        #progressionManagerOption = self.request_data['progressionManager']
+        finalOption = self.getBestInvestment(researchManagerOption,buildingManagerOption)
+        return finalOption
+    
+        
+    def getBestInvestment(self, research, building):
+        # Apes together strong
+        if self.isResearchable(research):
+            return research
+        elif self.isConstructable(building):
+            return building
+       # elif self.isProgressive(progression):
+       #    return progression
+        else: 
+            return {'Result':'None'}
+        
+
+
+    def isResearchable(self, research):
+        return (research['researchable']['researchID'] != -1)
+            
+    def isConstructable(self, building):
+        return (building['constructable']['buildingID'] != -1)
+
+    #def isProgressive(self, progression):                     
+    #    progBuilding = progression['constructable']
+    #    progResearch = progression['researchable']
+        #if (progBuilding['buildingID'] != -1 and self.isReasonable(progBuilding['buildingId'], progBuilding['buildingLevel'])):
+
+
+
+    #def isReasonable(self, ogameID, ogameLvl):
+        
+
+            
 
 investManager = investmentManager()
 
@@ -20,7 +61,8 @@ app = Flask(__name__)
 @app.route('/get_investment', methods=['GET'])
 def getInvestmentEndpoint(): 
     investManager.request_data = request.get_json()
-    
+    respData = investManager.getPreferredInvestmentJson()
+    return respData
 
 
 @app.route('/ready', methods=['GET'])
